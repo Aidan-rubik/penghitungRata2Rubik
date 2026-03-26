e2eaeb925ec7b57311dc49adf7dfe35a4419dd5f
Aidan-rubik <vikiaidanreneo@gmail.com>
2026-03-26 10:03:33 +0700

    minimalisirkan

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 document.addEventListener('alpine:init', () => {
             Alpine.store('rataRata' , {
-                hasilAoFive: '',
-                hasilMoThree: '',
+                hasilAoFive: '', hasilMoThree: '',
                 rataRataAofive(a,b,c,d,e){
-                    //waktu menjadi array
                     let waktu = [parseFloat(a), parseFloat(b), parseFloat(c), parseFloat(d), parseFloat(e)];
                     //pengurutan dari terbesar sampai terkecil
                     for (let i = 0; i < waktu.length; i++){
@@ -19,13 +17,8 @@ document.addEventListener('alpine:init', () => {
                     //menambah 3 waktu tengah lalu dibagikan 
                     this.hasilAoFive = (waktu[1] + waktu[2] + waktu[3]) / 3;
                 },
-                rataRataMothree(a,b,c){
-                    //menambah ketiga waktu tersebut lalu dibagi 3
-                    this.hasilMoThree = (parseFloat(a) + parseFloat(b) + parseFloat(c)) / 3;
-                },
+		//menambahkan ke-3 waktu lalu dibagi 3
+                rataRataMothree(a,b,c){ this.hasilMoThree = (parseFloat(a) + parseFloat(b) + parseFloat(c)) / 3; },
             }),
-            Alpine.store('modeGelap', {
-            on: false,
-            toggle() {this.on = !this.on}
-        })
+            Alpine.store('modeGelap', { on: false, toggle() {this.on = !this.on} })
     })
